e3c8efabda6f795260bd81ef7c452ebc04c28407
Vladyslav Liubchyk <lyuben1337@gmail.com>
2023-04-11 19:51:40 +0200

    Initial Commit

---
+++ src/main/java/com/example/tradeofferapi/TradeOfferApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tradeofferapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeOfferApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeOfferApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tradeofferapi/TradeOfferApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tradeofferapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeOfferApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
